eaf5a4451f9653abee6d5a113946708ac5bd74a2
Abdelrahman Arafat <darkinb255@gmail.com>
2020-11-19 00:26:42 +0200

    reward card styling done

--- src/components/rewards/RewardCard/index.jsx
+++ src/components/rewards/RewardCard/index.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
 
 import hotLabel from '@/assets/images/hot-label.svg'
-import calenderIcon from '@/assets/images/calender.svg'
+import calendar from '@/assets/images/calendar.svg'
 
-export default  ({CardInfo})=> {
+
+export default  ({title,id,icon,expDate,totalLockedValue,accuredRewards,apy,isHot})=> {
     return (
-        <div>
-            <h1>card</h1>
+        <div className="reward-card">
+            <div className="card-icons">
+                <img src={icon} id="card-icon"></img>
+                {isHot && <img src={hotLabel}></img>}
+            </div>
+            <h1 className="card-title">{title}</h1>
+            <div className="card-section">
+                <div className="card-calender-label">
+                <img className="card-calender-icon" src={calendar}></img>
+                    <h1 id="section-label">EXPIRE DATE</h1>
+                    </div>
+                <h1 id="section-info">{expDate}</h1>
+            </div>
+            <div className="card-section">
+                <h1 id="section-label">TOTAL LOCKED VALUE</h1>
+                <h1 id="section-info">{totalLockedValue}</h1>
+            </div>
+            <div className="card-section">
+                <h1 id="section-label">ACCURED REWARDS</h1>
+                <h1 id="section-info">{accuredRewards}</h1>
+            </div>
+            <div className="card-section">
+                <h1 id="section-label">APY</h1>
+                <h1 id="section-apy">{apy}</h1>
+            </div>
+          <div className="button-container">
+
+                <button className="card-button">Submit</button>
+          </div>
+   
         </div>
     )
 }
--- src/components/rewards/index.jsx
+++ src/components/rewards/index.jsx
@@ -1,14 +1,53 @@
 import React from 'react'
+import './rewards.css'
 import RewardCard from '@/components/rewards/RewardCard'
 import ethDaiIcon from '@/assets/images/coins-pair-eth-dai.svg'
 import ethUsdcIcon from '@/assets/images/coins-pair-eth-usdc.svg'
 import ethWbtcIcon from '@/assets/images/coins-pair-eth-wbtc.svg'
-
+import ethFuseIcon from '@/assets/images/coins-pair-eth-fuse.svg'
+const ethRewardCards = [
+    {
+        title:"ETH / FUSE",
+        icon:ethFuseIcon,
+        expDate:"16 November 2020",
+        totalLockedValue:"9,999,999.99 FUSE",
+        accuredRewards:"9,999,999.99 FUSE/BLOCK",
+        apy:"3.89%",
+        isHot:false
+    },
+    {
+        title:"ETH / FUSE",
+        icon:ethFuseIcon,
+        expDate:"16 November 2020",
+        totalLockedValue:"9,999,999.99 FUSE",
+        accuredRewards:"9,999,999.99 FUSE/BLOCK",
+        apy:"3.89%",
+        isHot:true
+    }
+];
 export default  ()=> {
+
     return (
-        <div>
-            <h1>Rewards</h1>
-            <RewardCard/>
+        <div className="rewards-page">
+            <div className="cards-container">
+
+            {
+                ethRewardCards.map(card=>(
+                    <RewardCard
+                    title = {card.title}
+                    id={card.id}
+                    key={card.id}
+                    icon = {card.icon}
+                    expDate = {card.expDate}
+                    totalLockedValue = {card.totalLockedValue}
+                    accuredRewards = {card.accuredRewards}
+                    apy = {card.apy}
+                    isHot = {card.isHot}
+                    />
+                ))
+            }
+            
+            </div>
         </div>
     )
 }
